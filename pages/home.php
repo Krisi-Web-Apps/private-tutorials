2bb12f0a9095f699821d20ebfd87ac5ad6ee55e3
Kristiyan <kris.bakalov98@gmail.com>
2023-11-25 02:31:36 +0200

    Updated the home.php

--- pages/home.php
+++ pages/home.php
@@ -6,4 +6,126 @@
   </div>
 </header>
 
+<main>
+  <div class="text-center my-20">
+    <h2 class="font-bold text-3xl">Как Мога Да Ви Помогна?</h2>
+    <p class="max-w-xl mx-auto mt-4 text-xl">Научете какви курсове предлагам за, Вашето развитие в областта на уеб
+      програмирането.</p>
+  </div>
+
+  <div class="container mx-auto">
+    <ul class="grid gap-10 md:grid-cols-2 xl:grid-cols-3">
+      <li class="rounded shadow-xl p-10">
+        <div class="max-w-[140px] max-h-[140px] rounded-full mx-auto overflow-hidden">
+          <img class="w-full h-full" src="/assets/images/illustrations/HTML-and-CSS-basics.jpg" alt="">
+        </div>
+        <h3 class="text-2xl font-bold text-center my-5">Основи на HTML</h3>
+        <p class="text-center">
+          Създавам и провеждам онлайн обучения под формата на уроци, фокусирани върху изграждането на уеб страници чрез
+          HTML. Моята мисия е да
+          помогна на хората не само освоят основите на HTML, но и да развият уменията си в уеб разработката. С
+          ентусиазъм и практически подход, уроците ми не само предоставят технически знания, но и помагат на учащите да
+          разберат контекста и важността на ефективното използване на HTML в съвременните уеб страници. Присъединете се
+          към моите уроци и заедно ще изграждаме стабилна основа на познанията Ви за уеб разработката.
+        </p>
+        <div class="text-center mt-5">
+          <a href="#" class="inline-block py-3 px-6 rounded text-white bg-slate-900">Научете повече</a>
+        </div>
+      </li>
+      <li class="rounded shadow-xl p-10">
+        <div class="max-w-[140px] max-h-[140px] rounded-full mx-auto overflow-hidden">
+          <img class="w-full h-full" src="/assets/images/illustrations/CSS-basics.jpg" alt="">
+        </div>
+        <h3 class="text-2xl font-bold text-center my-5">Основи на CSS</h3>
+        <p class="text-center">
+          Организирам и водя онлайн уроци, фокусирани върху изграждането на стил и дизайн на уеб страници чрез
+          CSS. Моята цел е да подкрепя учащите се не само в усвояването на основите на HTML & CSS, но и в развитието на
+          техните
+          умения в областта на уеб дизайна. Съчетавайки ентусиазъм и практически подход, моите уроци не само предоставят
+          техническите аспекти на CSS, но и помагат на хората да разберат контекста и важността на стиловете за
+          съвременното изграждане на уеб страници. Присъединете се към обученията ми и заедно ще развиваме вашия талант
+          в уеб дизайна и стилизирането.
+        </p>
+        <div class="text-center mt-5">
+          <a href="#" class="inline-block py-3 px-6 rounded text-white bg-slate-900">Научете повече</a>
+        </div>
+      </li>
+      <li class="rounded shadow-xl p-10">
+        <div class="max-w-[140px] max-h-[140px] rounded-full mx-auto overflow-hidden">
+          <img class="w-full h-full" src="/assets/images/illustrations/JavaScript-basics.jpg" alt="">
+        </div>
+        <h3 class="text-2xl font-bold text-center my-5">Основи на JavaScript</h3>
+        <p class="text-center">
+          Провеждам онлайн уроци, съсредоточени върху използването на JavaScript за надграждане на
+          функционалността и интерактивността на една уеб страница. Моята цел е да насърча учащите не само да усвоят
+          основите на JavaScript, но и да развият техните програмистки умения в областта на уеб разработката. С
+          ентусиазъм, практически подход и увереност, моите уроци не само предоставят фундаментални знания по
+          JavaScript, но и
+          помагат на хората да разберат контекста и значението на този език за по-модерното програмиране в уеб сайтовете
+          и приложенията.
+          Присъединете се към моите уроци и заедно ще изграждаме стабилна основа за успешното изполжване на JavaScript и
+          разработка на динамични уеб приложения.
+        </p>
+        <div class="text-center mt-5">
+          <a href="#" class="inline-block py-3 px-6 rounded text-white bg-slate-900">Научете повече</a>
+        </div>
+      </li>
+      <li class="rounded shadow-xl p-10">
+        <div class="max-w-[140px] max-h-[140px] rounded-full mx-auto overflow-hidden">
+          <img class="w-full h-full" src="/assets/images/illustrations/PHP-basics.png" alt="">
+        </div>
+        <h3 class="text-2xl font-bold text-center my-5">Основи на PHP</h3>
+        <p class="text-center">
+          Водя онлайн уроци, съсредоточени върху основите на PHP и тяхното приложение в уеб разработката. Целта ми е да
+          предоставя на студентите не само технически познания по PHP, но и да им помогна да развият практически
+          умения, необходими за изграждане на базови динамични уеб страници и приложения. С професионален
+          преподавателски подход и ентусиазъм,
+          уроците ми се фокусират не само върху основите на PHP, но и върху възможностите, които този език предоставя за
+          разнообразни уеб проекти. Присъединете се към уроците ми и заедно ще изграждаме знания и умения за успешна
+          употреба на PHP в уеб разработката.
+        </p>
+        <div class="text-center mt-5">
+          <a href="#" class="inline-block py-3 px-6 rounded text-white bg-slate-900">Научете повече</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+  <div class="container mx-auto">
+    <section class="mt-20">
+      <div class="grid gap-10 md:grid-cols-2 items-center">
+        <img src="/assets/images/why-trust-me-img.jpg" alt="">
+        <div>
+          <h2 class="text-slate-900 text-4xl font-bold">Защо да се доверите на мен?</h2>
+          <p class="text-xl py-10">
+            Можете да се на мен като преподавател, защото не само притежавам богат опит и знание в областта на
+            програмирането, но и
+            създавам учебна среда, където ученето става вдъхновяващо и практично приключение. С посветеност и
+            индивидуален подход, ще ви насоча към усвояване на материала, като ви вдъхновявам да прилагате наученото в
+            реални проекти. Работейки заедно, ще усвоите фундаментални знания и умения, за успешно справяне на
+            предизвикателствата в уеб разработката.
+          </p>
+          <p class="text-xl">
+            Моят подход към обучението е ориентиран към практическото изграждане на знания и умения, които всъщност са
+            по-важни от теоретичната част в нашата област.
+            Съчетавам традиционни методи с иновативни техники, за да осигурявам интерактивни и ангажиращи уроци. Вярвам,
+            че усвояването на нови умения трябва да бъде вълнуващо пътешествие, поради което създавам стимулираща
+            обстановка, която подпомага вашата индивидуална креативност и растеж.
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <section class="mt-20 py-20 contact-img">
+    <h2 class="text-white text-center font-bold text-4xl">Свържете се с мен</h2>
+    <p class="text-white my-10 text-xl max-w-xl mx-auto text-center">Направете първата стъпка към по-лесно и успешно
+      овладяване на уеб програмирането.</p>
+    <div class="text-center">
+      <a href="/contacts" class="inline-block py-6 px-14 rounded text-white bg-slate-900 text-xl">Контакти</a>
+    </div>
+  </section>
+
+</main>
+
 <?php require "inc/footer.php"; ?>
